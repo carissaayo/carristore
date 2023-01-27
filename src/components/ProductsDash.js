0d6689fbb485159e0320aeb192d3a6fc6553b73d
Yussuf <you@example.com>
2023-01-27 11:31:33 +0100

    done with product page

--- src/components/ProductsDash.js
+++ src/components/ProductsDash.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductItem from './ProductItem';
+import ProductsCon from './ProductsCon';
 
 const ProductsDash = () => {
   return (
@@ -12,19 +13,7 @@ const ProductsDash = () => {
         </button>
       </div>
 
-      {/* Product Main Header */}
-      <div className="flex items-center  w-full bg-[#F5F5F5] px-4 py-3 border rounded-md font-medium mb-4 justify-between gap-14 sm:gap-4 lg:gap-0 ">
-        <p className="w-[70%] xs:w-[30%] md:w-[20%] lg:w-[17%]">Item</p>
-        <p className=" hidden md:block w-[10%]">Views</p>
-        <p className="hidden xs:block w-[30%] md:w-[17%]">Price</p>
-        <p className="hidden sm:block w-[20%] md:w-[15%]">Category</p>
-        <p className="w-[30%] md:w-[15%]">Actions</p>
-        <p className="hidden md:block w-[16%]">Availability</p>
-      </div>
-
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
+     <ProductsCon/>
     </main>
   );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,178 +1,63 @@
-import {
-  ArrowTrendingUpIcon,
-  ChevronDownIcon,
-  EyeIcon,
-  KeyIcon,
-  UserIcon,
-} from "@heroicons/react/24/outline";
 import { useState } from "react";
+import {MagnifyingGlassIcon,} from "@heroicons/react/24/outline";
 import SideNav from "../components/landingpageComponents/SideNav";
 import PageHeader from "../components/PageHeader";
 
 import VerifyEmailText from "../components/VerifyEmailText";
+import ProductsCon from "../components/ProductsCon";
 
 const Products = () => {
   const [openSideNav, setOpenSideNav] = useState(false);
-  const [activeCon, setActiveCon] = useState(1);
+
 
   return (
     <main className="xl:flex h-screen w-full bg-secondaryColor gap-4 py-4 px-2 sm:px-4 overflow-hidden relative">
       {/* Overlay Starts*/}
       <div
         className={`${
-          openSideNav &&
-          "w-full h-screen  absolute top-0 left-0      bg-[#00000070]"
+          openSideNav && "w-full h-screen  absolute top-0 left-0 bg-[#00000070]"
         } xl:hidden`}
         onClick={() => setOpenSideNav(false)}
       ></div>
       {/* Overlay Ends */}
       <SideNav openSideNav={openSideNav} setOpenSideNav={setOpenSideNav} />
       <section className="flex-[4]  bg-white rounded-lg ">
-        <PageHeader title="My Profile" setOpenSideNav={setOpenSideNav} />
+        <PageHeader title="Products" setOpenSideNav={setOpenSideNav} />
         <VerifyEmailText />
 
-        <section className=" flex flex-col md:flex-row px-6 py-4 gap-8 md:gap-20 lg:pr-20  h-[77vh] sm:h-[75vh] md:h-[78vh] xl:h-auto ">
-          <section className="md:flex-1 sm:flex  md:block">
-            <button
-              className={`flex flex-1 gap-4  items-center px-3 py-3  w-full sm:min-w-[250px] border-l-2  ${
-                activeCon === 1
-                  ? "border-black bg-[#E9E9E9]"
-                  : "border-[#00000020]"
-              }`}
-              onClick={() => setActiveCon(1)}
-            >
-              <div
-                className={`w-8 h-8 rounded-full flex items-center justify-center  ${
-                  activeCon === 1 && "bg-[#2100EA] text-white"
-                }`}
-              >
-                <UserIcon className="w-6 h-6 " />
-              </div>
-              <h4 className="font-medium text-lg">Basic Details</h4>
-            </button>
-
-            <button
-              className={`flex-1 flex gap-4  items-center px-3 py-3 w-full sm:min-w-[250px] border-l-2  ${
-                activeCon === 2
-                  ? "border-black bg-[#E9E9E9]"
-                  : "border-[#00000020]"
-              }`}
-              onClick={() => setActiveCon(2)}
-            >
-              <div
-                className={`w-8 h-8 rounded-full flex items-center justify-center ${
-                  activeCon === 2 && "bg-[#2100EA] text-white"
-                }`}
-              >
-                <KeyIcon className="w-5 h-5" />
-              </div>
-              <h4 className="font-medium ">Update Password</h4>
-            </button>
-          </section>
-          <section className="md:flex-[4] relative  ">
-            {/* Update Password */}
-            <div className={`${activeCon === 1 && "absolute left-[200%]"} `}>
-              <div className="flex gap-4 bg-[#E9E9E9] items-center px-8 py-3  mb-8 sm:mb-20">
-                <h4 className="font-medium text-lg">Edit Password</h4>
-              </div>
-
-              {/* Input Con */}
-              <div className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8">
-                <div className="w-full border h-[50px] rounded-lg flex items-center gap-3">
-                  <input
-                    type="text"
-                    className="w-[90%] h-full rounded-lg outline-none pl-3  "
-                    placeholder="Current Password"
-                  />
-                  <EyeIcon className="w-4 h-6 cursor-pointer" />
-                </div>
-              </div>
-
-              {/* Input Con */}
-              <div className=" w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8">
-                <div className="w-full border h-[50px] rounded-lg flex items-center gap-3">
-                  <input
-                    type="text"
-                    className="w-[90%] h-full rounded-lg outline-none pl-3  "
-                    placeholder="New Password"
-                  />
-                  <EyeIcon className="w-4 h-6 cursor-pointer" />
-                </div>
-              </div>
-
-              {/* Input Con */}
-              <div className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8">
-                <div className="w-full border h-[50px] rounded-lg flex items-center gap-3">
-                  <input
-                    type="text"
-                    className="w-[90%] h-full rounded-lg outline-none pl-3  "
-                    placeholder="Confirm New Password"
-                  />
-                  <EyeIcon className="w-4 h-6 cursor-pointer" />
-                </div>
-              </div>
-
-              <button className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] bg-[#0011AC] h-[50px] flex items-center justify-center mx-auto rounded-md text-white text-lg hover:scale-110">
-                Update Password
-              </button>
+        <section className="w-full px-6 h-[78vh] xl:h-auto">
+          <section className="flex sm:items-center justify-between mb-8 flex-col sm:flex-row gap-6 sm:gap-0">
+            <div className="">
+              <h2 className="font-bold text-xl">All Products</h2>
             </div>
-
-            {/* Basic Details */}
-            <div className={`${activeCon === 2 && "absolute left-[200%]"}`}>
-              <div className="flex gap-4 bg-[#E9E9E9] items-center px-8 py-3  mb-8 sm:mb-20">
-                <h4 className="font-medium text-lg">Basic Details</h4>
-              </div>
-
-              {/* Input Con */}
-              <div className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8">
-                <div className="w-full border h-[50px] rounded-lg relative">
-                  <span className="absolute top-[-38%] left-[10%] text-[#00000060] bg-white text-sm">
-                    Name
-                  </span>
+            <div className="flex items-center gap-6">
+              {/* Search Con */}
+              <div className="w-full sm:min-w-[300px]">
+                <div className="w-full border h-[40px] rounded-lg flex items-center gap-3 pr-1">
                   <input
                     type="text"
-                    className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] "
-                    placeholder="Yussuf Ajao"
+                    className="w-[90%] h-full rounded-lg outline-none pl-3  "
+                    placeholder="Search items"
                   />
+                  <div className="flex items-center justify-center w-8 h-8 bg-[#E9E9E9] rounded-md">
+                    <MagnifyingGlassIcon className="w-4 h-6 cursor-pointer  opacity-75" />
+                  </div>
                 </div>
               </div>
-
-              {/* Input Con */}
-              <div className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8 ">
-                <div className="w-full border h-[50px] rounded-lg relative ">
-                  <span className="absolute top-[-38%] left-[10%] text-[#00000060] bg-white text-sm ">
-                    Email
-                  </span>
-                  <input
-                    type="text"
-                    className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] "
-                    placeholder="ajaoyussuf@gmail.com"
-                  />
-                </div>
-              </div>
-
-              {/* Input Con */}
-              <div className=" w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] mx-auto mb-8 ">
-                <div className="w-full border h-[50px] rounded-lg relative flex items-center pl-2 gap-4">
-                  <span className="absolute top-[-38%] left-[10%] text-[#00000060] bg-white text-sm">
-                    Phone Number
-                  </span>
-                  <p className="flex items-center gap-1">
-                    +234 <ChevronDownIcon className="w-4 h-4" />
-                  </p>
-                  <input
-                    type="text"
-                    className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] "
-                    placeholder="ajaoyussuf@gmail.com"
-                  />
-                </div>
-              </div>
-
-              <button className="w-[95%] xs:w-[80%] sm:w-[70%] md:w-[80%] xl:w-[60%] bg-[#0011AC] h-[50px] flex items-center justify-center mx-auto rounded-md text-white text-lg hover:scale-110">
-                Update basic details
+              <button className="min-w-[200px] hidden md:flex items-center justify-center h-[40px] rounded-md bg-[#0011AC] text-white hover:scale-105">
+                Add New Products{" "}
               </button>
             </div>
           </section>
+
+          {/* Products Con */}
+          <ProductsCon />
+
+          <div className="w-full flex items-center justify-end pt-8">
+            <button className="min-w-[200px] flex md:hidden items-center justify-center h-[40px] rounded-md bg-[#0011AC] text-white hover:scale-105">
+              Add New Products{" "}
+            </button>
+          </div>
         </section>
       </section>
     </main>
