8ac5531a1484fc7b1027144da704689e23ddc5d6
Yussuf <you@example.com>
2023-01-29 12:21:28 +0100

    done with the addproduct 3

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import { MagnifyingGlassIcon, PlusIcon } from "@heroicons/react/24/outline";
+import {  CheckIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import SideNav from "../components/landingpageComponents/SideNav";
 import PageHeader from "../components/PageHeader";
 import VerifyEmailText from "../components/VerifyEmailText";
-import ProductsCon from "../components/ProductsCon";
-import EditProduct from "../components/EditProduct";
-import { Link } from "react-router-dom";
+import foodImg from "../assets/images/foood.webp"
 
 const AddProduct = () => {
      const [openSideNav, setOpenSideNav] = useState(false);
@@ -29,36 +27,123 @@ const AddProduct = () => {
         <PageHeader title="Add Products" setOpenSideNav={setOpenSideNav} />
         <VerifyEmailText />
 
-        <section className="w-full px-4 xs:px-6 h-[74vh] xl:h-auto md:flex md:flex-col items-center justify-center">
-          <div className="w-[95%] xs:w-[90%] sm:w-[80%] md:w-[70%] xl:w-[50%] text-center">
-            <h1 className="font-bold  xs:text-xl mb-6">
-              Add images - for multiple products
+        {/* Add Product 3 */}
+
+        <section className=" w-full h-[76vh] md:h-[78vh] overflow-y-scroll pb-10">
+          <div className="w-full px-2 xs:px-0 xs:w-[80%] sm:w-[70%] md:w-[60%] xl:w-[50%] xs:mx-auto ">
+            {/* First Image con */}
+            <div className="bg-white flex justify-center mb-2">
+              <div className="w-[100px] h-[100px]  rounded-2xl mb-3 ">
+                <img src={foodImg} alt="" className="h-full w-full" />
+              </div>
+            </div>
+
+            <h1 className="text-xl font-bold text-center mb-2">
+              Add Details - Product 1
             </h1>
 
-            <p className="flex items-center gap-2  font-medium mb-20 text-[#F84A4A] sm:text-black text-sm xs:text-base">
-              <span className="hidden sm:flex items-center justify-center bg-[#F84A4A] p-2 text-white rounded-xl px-4">
-                Important
-              </span>
-              Pick one image for every product you want to add (max: 10) ,
-              you’ll be able to add more images later -
-            </p>
-
-            <section className="flex relative items-center mb-32 justify-center">
-              <div className="relative left-[12%] rotate-[-280deg]">
-                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
-              </div>
-              <div className="bg-white z-20">
-                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative cursor-pointer   mb-3 border-dotted border-2 border-black flex items-center justify-center">
-                  <PlusIcon className="w-12 h-12 text-[#00000070]" />
+            <div className="w-full flex items-center justify-center mb-8">
+              <button className="text-[#FF2828] text-sm flex items-center justify-center gap-1 cursor-pointer">
+                Remove Item <TrashIcon className="w-4 h-4" />
+              </button>
+            </div>
+            <div className="w-full mb-8">
+              <section className="px-6 mb-6 h-[]">
+                {/* Images Con */}
+                <div className="w-full  flex gap-10 mb-4">
+                  {/* Image 1 */}
+                  <div className="">
+                    <div className="w-[100px] h-[100px] rounded-2xl relative cursor-pointer   mb-3">
+                      <img
+                        src={foodImg}
+                        alt=""
+                        className="w-full h-full rounded-2xl"
+                      />
+                      <div className="absolute top-0 left-0 bg-[#00000080] z-10 h-full w-full flex items-center justify-center rounded-2xl">
+                        <CheckIcon className="w-12 h-12 text-green-400 " />
+                      </div>
+                    </div>
+                    <div className="w-[98px] h-[3px] bg-green-400"></div>
+                  </div>
+                  {/* Image 2 */}
+                  <div className="">
+                    <div className="w-[100px] h-[100px] rounded-2xl relative cursor-pointer   mb-3 border-dotted border-2 flex items-center justify-center">
+                      <PlusIcon className="w-12 h-12 text-[#00000070]" />
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="relative right-[12%] rotate-[10deg] bottom-[-7px]">
-                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
-              </div>
-            </section>
-            <div className="w-full flex items-center justify-center">
-              <button className="bg-[#0011AC] text-white px-4 py-2 w-[80%] md:w-[60%] hover:scale-105">
-                Proceed
+                <p className="text-[#00000080]">
+                  Click on any image to change thumbnail
+                </p>
+              </section>
+              <section className="px-6 w-full">
+                {/* Input Con */}
+                <div className="w-[95%] mb-8 mx-auto">
+                  <div className="w-full border h-[50px] rounded-lg relative">
+                    <span className="absolute top-[-18%] left-[5%] text-[#00000060] bg-white text-sm">
+                      Product Name
+                    </span>
+                    <input
+                      type="text"
+                      className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] bg-[#ECECEC42] "
+                      placeholder="Quality Jeans"
+                    />
+                  </div>
+                </div>
+
+                {/* Input Con */}
+                <div className="w-[95%] mb-8 mx-auto">
+                  <div className="w-full border h-[50px] rounded-lg relative">
+                    <span className="absolute top-[-18%] left-[5%] text-[#00000060] bg-white text-sm">
+                      Product Price in NGN
+                    </span>
+                    <input
+                      type="text"
+                      className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] bg-[#ECECEC42] "
+                      placeholder="Quality Jeans"
+                    />
+                  </div>
+                  {/* Discount */}
+                  <div className="flex gap-2 items-center"></div>
+                </div>
+
+                {/* Input Con */}
+                <div className="w-[95%] mb-8 mx-auto">
+                  <div className="w-full border h-[50px] rounded-lg relative">
+                    <span className="absolute top-[-18%] left-[5%] text-[#00000060] bg-white text-sm">
+                      Select Product Category
+                    </span>
+                    <input
+                      type="text"
+                      className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] bg-[#ECECEC42] "
+                      placeholder="Quality Jeans"
+                    />
+                  </div>
+                </div>
+
+                {/* Input Con */}
+                <div className="w-[95%] mb-8 mx-auto">
+                  <div className="w-full border h-[100px] rounded-lg relative">
+                    <span className="absolute top-[-18%] left-[5%] text-[#00000060] bg-white text-sm">
+                      Product Description
+                    </span>
+                    <textarea
+                      name=""
+                      id=""
+                      cols="30"
+                      rows="10"
+                      className="w-full h-full rounded-lg outline-none pl-3 focus:outline-2 focus:outline-[#00000060] bg-[#ECECEC42]"
+                    ></textarea>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="w-[90%] mx-auto flex justify-between">
+              <button className="w-[48%] text-[#0011AC] bg-[#E2E2E2] py-4 hover:scale-105 rounded-xl">
+                Back To Images
+              </button>
+              <button className="w-[48%] bg-[#0011AC] text-white py-4 hover:scale-105 rounded-xl">
+                Upload Product
               </button>
             </div>
           </div>
--- src/pages/PageCon.js
+++ src/pages/PageCon.js
@@ -22,13 +22,12 @@ const PageCon = ({page}) => {
       {/* Overlay Ends */}
       <SideNav openSideNav={openSideNav} setOpenSideNav={setOpenSideNav} />
       <section className="flex-[4]  bg-white rounded-lg ">
-
         <PageHeader setOpenSideNav={setOpenSideNav} />
         <VerifyEmailText />
 
         {/* Scroll Part Starts */}
         <section className=" h-[75vh] overflow-y-scroll ">
-            {page}
+          {page}
           {/* details con */}
           <div className="flex flex-wrap lg:flex-nowrap items-center justify-between w-[95%] mx-auto border rounded-xl lg:rounded-full bg-white px-4 xs:px-8 py-4 lg:py-0  mb-4">
             {/* First Child */}
@@ -89,6 +88,73 @@ const PageCon = ({page}) => {
           <ProductsDash />
         </section>
         {/* Scroll Part Ends */}
+      </section>
+
+      {/* Add Product 2 */}
+      <section className="w-full px-4 xs:px-6 h-[74vh] xl:h-auto md:flex md:flex-col items-center justify-center">
+        <div className="w-[95%] xs:w-[90%] sm:w-[80%] md:w-[70%] xl:w-[50%] text-center">
+          <h1 className="font-bold  xs:text-xl mb-6">
+            Add images - for multiple products
+          </h1>
+
+          <p className="flex items-center gap-2  font-medium mb-20 text-[#F84A4A] sm:text-black text-sm xs:text-base">
+            <span className="hidden sm:flex items-center justify-center bg-[#F84A4A] p-2 text-white rounded-xl px-4">
+              Important
+            </span>
+            Pick one image for every product you want to add (max: 10) , you’ll
+            be able to add more images later -
+          </p>
+
+          <section className="flex relative items-center mb-32 justify-center">
+            <div className="bg-white ">
+              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl mb-3 relative">
+                <img src={foodImg} alt="" className="h-full w-full" />
+                <div className="w-full h-full bg-[#00000050] absolute top-0"></div>
+              </div>
+            </div>
+          </section>
+          <div className="w-full flex items-center justify-center">
+            <button className="bg-[#0011AC] text-white px-4 py-2 w-[80%] md:w-[60%] hover:scale-105">
+              Proceed
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {/* Add Product 1 */}
+      <section className="w-full px-4 xs:px-6 h-[74vh] xl:h-auto md:flex md:flex-col items-center justify-center">
+        <div className="w-[95%] xs:w-[90%] sm:w-[80%] md:w-[70%] xl:w-[50%] text-center">
+          <h1 className="font-bold  xs:text-xl mb-6">
+            Add images - for multiple products
+          </h1>
+
+          <p className="flex items-center gap-2  font-medium mb-20 text-[#F84A4A] sm:text-black text-sm xs:text-base">
+            <span className="hidden sm:flex items-center justify-center bg-[#F84A4A] p-2 text-white rounded-xl px-4">
+              Important
+            </span>
+            Pick one image for every product you want to add (max: 10) , you’ll
+            be able to add more images later -
+          </p>
+
+          <section className="flex relative items-center mb-32 justify-center">
+            <div className="relative left-[12%] rotate-[-280deg]">
+              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
+            </div>
+            <div className="bg-white z-10">
+              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative cursor-pointer   mb-3 border-dotted border-2 border-black flex items-center justify-center">
+                <PlusIcon className="w-12 h-12 text-[#00000070]" />
+              </div>
+            </div>
+            <div className="relative right-[12%] rotate-[10deg] bottom-[-7px]">
+              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
+            </div>
+          </section>
+          <div className="w-full flex items-center justify-center">
+            <button className="bg-[#0011AC] text-white px-4 py-2 w-[80%] md:w-[60%] hover:scale-105">
+              Proceed
+            </button>
+          </div>
+        </div>
       </section>
     </main>
   );
