7c66a5362dface5d2a5ed7041200cabb8efc53fa
Yussuf <you@example.com>
2023-01-29 12:33:41 +0100

    Done with the first two parts of add product

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -8,6 +8,7 @@ import foodImg from "../assets/images/foood.webp"
 const AddProduct = () => {
      const [openSideNav, setOpenSideNav] = useState(false);
      const [openEditProduct, setOpenEditProduct] = useState(false);
+     const [currentAddPage, setCurrentAddPage] = useState(1)
   return (
     <main className="xl:flex h-screen w-full bg-secondaryColor gap-4 py-4 px-2 sm:px-4 overflow-hidden relative">
       {/* Overlay Starts*/}
@@ -23,13 +24,59 @@ const AddProduct = () => {
         setOpenSideNav={setOpenSideNav}
         setOpenEditProduct={setOpenEditProduct}
       />
-      <section className="flex-[4]  bg-white rounded-lg ">
+      <section className="flex-[4]  bg-white rounded-lg relative">
         <PageHeader title="Add Products" setOpenSideNav={setOpenSideNav} />
         <VerifyEmailText />
 
-        {/* Add Product 3 */}
+        {/* Add Product 1 */}
+        <section
+          className={`w-full px-4 xs:px-6 h-[74vh] xl:h-auto md:flex md:flex-col items-center justify-center transistion-translate duration-500 ease-in-out ${
+            currentAddPage === 2 && "translate-x-[-120%] absolute "
+          }`}
+        >
+          <div className="w-[95%] xs:w-[90%] sm:w-[80%] md:w-[70%] xl:w-[50%] text-center">
+            <h1 className="font-bold  xs:text-xl mb-6">
+              Add images - for multiple products
+            </h1>
 
-        <section className=" w-full h-[76vh] md:h-[78vh] overflow-y-scroll pb-10">
+            <p className="flex items-center gap-2  font-medium mb-20 text-[#F84A4A] sm:text-black text-sm xs:text-base">
+              <span className="hidden sm:flex items-center justify-center bg-[#F84A4A] p-2 text-white rounded-xl px-4">
+                Important
+              </span>
+              Pick one image for every product you want to add (max: 10) ,
+              you’ll be able to add more images later -
+            </p>
+
+            <section className="flex relative items-center mb-32 justify-center">
+              <div className="relative left-[12%] rotate-[-280deg]">
+                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
+              </div>
+              <div className="bg-white z-10">
+                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative cursor-pointer   mb-3 border-dotted border-2 border-black flex items-center justify-center">
+                  <PlusIcon className="w-12 h-12 text-[#00000070]" />
+                </div>
+              </div>
+              <div className="relative right-[12%] rotate-[10deg] bottom-[-7px]">
+                <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
+              </div>
+            </section>
+            <div className="w-full flex items-center justify-center">
+              <button
+                className="bg-[#0011AC] text-white px-4 py-2 w-[80%] md:w-[60%] hover:scale-105"
+                onClick={() => setCurrentAddPage(2)}
+              >
+                Proceed
+              </button>
+            </div>
+          </div>
+        </section>
+
+        {/* Add Product 3 */}
+        <section
+          className={` w-full h-[76vh] md:h-[78vh] overflow-y-scroll pb-10 transistion-translate duration-500 ease-in-out ${
+            currentAddPage === 1 && "translate-x-[120%] absolute "
+          }`}
+        >
           <div className="w-full px-2 xs:px-0 xs:w-[80%] sm:w-[70%] md:w-[60%] xl:w-[50%] xs:mx-auto ">
             {/* First Image con */}
             <div className="bg-white flex justify-center mb-2">
@@ -139,7 +186,10 @@ const AddProduct = () => {
               </section>
             </div>
             <div className="w-[90%] mx-auto flex justify-between">
-              <button className="w-[48%] text-[#0011AC] bg-[#E2E2E2] py-4 hover:scale-105 rounded-xl">
+              <button
+                className="w-[48%] text-[#0011AC] bg-[#E2E2E2] py-4 hover:scale-105 rounded-xl"
+                onClick={() => setCurrentAddPage(1)}
+              >
                 Back To Images
               </button>
               <button className="w-[48%] bg-[#0011AC] text-white py-4 hover:scale-105 rounded-xl">
--- src/pages/PageCon.js
+++ src/pages/PageCon.js
@@ -120,42 +120,6 @@ const PageCon = ({page}) => {
           </div>
         </div>
       </section>
-
-      {/* Add Product 1 */}
-      <section className="w-full px-4 xs:px-6 h-[74vh] xl:h-auto md:flex md:flex-col items-center justify-center">
-        <div className="w-[95%] xs:w-[90%] sm:w-[80%] md:w-[70%] xl:w-[50%] text-center">
-          <h1 className="font-bold  xs:text-xl mb-6">
-            Add images - for multiple products
-          </h1>
-
-          <p className="flex items-center gap-2  font-medium mb-20 text-[#F84A4A] sm:text-black text-sm xs:text-base">
-            <span className="hidden sm:flex items-center justify-center bg-[#F84A4A] p-2 text-white rounded-xl px-4">
-              Important
-            </span>
-            Pick one image for every product you want to add (max: 10) , you’ll
-            be able to add more images later -
-          </p>
-
-          <section className="flex relative items-center mb-32 justify-center">
-            <div className="relative left-[12%] rotate-[-280deg]">
-              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
-            </div>
-            <div className="bg-white z-10">
-              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative cursor-pointer   mb-3 border-dotted border-2 border-black flex items-center justify-center">
-                <PlusIcon className="w-12 h-12 text-[#00000070]" />
-              </div>
-            </div>
-            <div className="relative right-[12%] rotate-[10deg] bottom-[-7px]">
-              <div className="w-[80px] h-[80px] sm:w-[150px] sm:h-[150px] rounded-2xl relative    mb-3 border-dotted border-2 flex items-center justify-center"></div>
-            </div>
-          </section>
-          <div className="w-full flex items-center justify-center">
-            <button className="bg-[#0011AC] text-white px-4 py-2 w-[80%] md:w-[60%] hover:scale-105">
-              Proceed
-            </button>
-          </div>
-        </div>
-      </section>
     </main>
   );
 };
